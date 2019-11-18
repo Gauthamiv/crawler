6c1f05fc312de20500af066a93f5a22bce2f85b3
gauthamiv <gauthami.viswanathan@searchblox.com>
2019-11-18 17:45:24 +0530

    version changes1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Crawler.Mixfile do
     [
       app:               :crawler,
       version:           "1.1.0",
-      elixir:            "~> 1.5",
+      elixir:            "~> 1.9",
       package:           package(),
       name:              "Crawler",
       description:       "A high performance web crawler in Elixir.",
@@ -29,7 +29,7 @@ defmodule Crawler.Mixfile do
   defp deps do
     [
       {:httpoison,   "~> 1.5"},
-      {:floki,       "~> 0.18"},
+      {:floki,       "~> 0.23"},
       {:opq,         "~> 3.0"},
       {:retry,       "~> 0.10"},
       {:plug_cowboy, "~> 1.0",   only: :test},

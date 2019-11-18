83461b73c23f83d607bf1ae4841b9fd222bc7101
gauthamiv <gauthami.viswanathan@searchblox.com>
2019-11-18 16:51:27 +0530

    minor change

--- lib/crawler/fetcher.ex
+++ lib/crawler/fetcher.ex
@@ -50,8 +50,8 @@ defmodule Crawler.Fetcher do
   end
 
   defp fetch_url_failed(reason, opts) do
-    rea = "#{reason}" |> Tuple.to_list |> Enum.join(", ")
-    {:error, "Failed to fetch #{opts[:url]}, reason: #{rea}"}
+    #rea = "#{reason}" |> Tuple.to_list |> Enum.join(", ")
+    {:error, "Failed to fetch #{opts[:url]}, reason: #{reason}"}
   end
 
   defp record_referrer_url(opts) do
